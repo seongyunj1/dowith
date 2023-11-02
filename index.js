ce3c64e3cfe5089276152385c013ca5421f4e156
seongyunj1 <149452926+seongyunj1@users.noreply.github.com>
2023-11-03 05:56:54 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".slide").slick({
+        // Infinity:true,
+        // dots:true,
+        // arrows:true,
+        // autoplay:true,
+        // autoplays:1000,
+        // fade:true,
+        // nav:true,
+        dots: true,
+		autoplay : true,
+    });
+});
